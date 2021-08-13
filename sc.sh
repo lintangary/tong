16aa0672e44aa9e5fb442505d3dddf1793a6ee4b
lintangary <88912327+lintangary@users.noreply.github.com>
2021-08-14 06:43:11 +0700

    Add files via upload

---
+++ sc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt update
+sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
+tar -xvf lolMiner_v1.31_Lin64.tar.gz  
+1.31/lolMiner --algo ETHASH --pool daggerhashimoto.usa-west.nicehash.com:3353 --user 3MJGyhnDxravChWdQ9P7NHQEtteM8QyLeD.Lintang
+
+
